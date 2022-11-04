96d586b2fa68971c0b4c2da225e7fdbfa82ea04f
Jeff Liang <JeffLiang@users.noreply.huggingface.co>
2022-11-04 13:51:26 +0000

    add colab link (#2)
    - add colab link (91bcee7f835471349ecdcb553cdbd77ceae06b83)
    
    
    Co-authored-by: Ahsen Khaliq <akhaliq@users.noreply.huggingface.co>

--- app.py
+++ app.py
@@ -63,6 +63,7 @@ description = """
 Gradio Demo for Open-Vocabulary Semantic Segmentation with Mask-adapted CLIP. \n
 OVSeg could perform open vocabulary segmentation, you may input more classes (seperate by comma). You may click on of the examples or upload your own image. \n
 It might take some time to process. Cheers!
+<p>Don't want to wait in queue? <a href="https://colab.research.google.com/drive/1O4Ain5uFZNcQYUmDTG92DpEGCatga8K5?usp=sharing"><img data-canonical-src="https://colab.research.google.com/assets/colab-badge.svg" alt="Open In Colab" src="https://camo.githubusercontent.com/84f0493939e0c4de4e6dbe113251b4bfb5353e57134ffd9fcab6b8714514d4d1/68747470733a2f2f636f6c61622e72657365617263682e676f6f676c652e636f6d2f6173736574732f636f6c61622d62616467652e737667"></a></p>
 """
 
 article = """
