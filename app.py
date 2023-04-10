f037852540f98a919004539dbf6fb0105c0ff339
JeffLiang <jeffliang@utexas.edu>
2023-04-09 23:15:45 -0500

    add ckpts

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ from detectron2.config import get_cfg
 from detectron2.projects.deeplab import add_deeplab_config
 from detectron2.data.detection_utils import read_image
 from open_vocab_seg import add_ovseg_config
-from open_vocab_seg.utils import VisualizationDemo
+from open_vocab_seg.utils import VisualizationDemo, SAMVisualizationDemo
 
 import gradio as gr
 
@@ -38,13 +38,14 @@ def setup_cfg(config_file):
     return cfg
 
 
-def inference(class_names, input_img):
+def inference(class_names, proposal_gen, granularity, input_img):
     mp.set_start_method("spawn", force=True)
     config_file = './ovseg_swinB_vitL_demo.yaml'
     cfg = setup_cfg(config_file)
-
-    demo = VisualizationDemo(cfg)
-
+    if proposal_gen == 'MaskFormer':
+        demo = VisualizationDemo(cfg)
+    elif proposal_gen == 'Segment_Anything':
+        demo = SAMVisualizationDemo(cfg, granularity, './sam_vit_h_4b8939.pth', './ovseg_clip_l_9a1909.pth')
     class_names = class_names.split(',')
     img = read_image(input_img, format="BGR")
     _, visualized_output = demo.run_on_image(img, class_names)
@@ -52,18 +53,21 @@ def inference(class_names, input_img):
     return Image.fromarray(np.uint8(visualized_output.get_image())).convert('RGB')
 
 
-examples = [['Oculus, Ukulele', './resources/demo_samples/sample_03.jpeg'],
-            ['Saturn V, toys, blossom', './resources/demo_samples/sample_01.jpeg'],
-            ['Golden gate, yacht', './resources/demo_samples/sample_02.jpeg'],]
+examples = [['Saturn V, toys, desk, sunflowers, white roses, chrysanthemums, carnations, green dianthus', 'Segment_Anything', 0.8, './resources/demo_samples/sample_01.jpeg'],
+            ['red bench, yellow bench, black bench, blue bench, brown bench, green bench, red chair, blue chair, yellow chair, green chair', 'Segment_Anything', 0.8, './resources/demo_samples/sample_04.png'],
+            ['Saturn V, toys, blossom', 'MaskFormer', 1.0, './resources/demo_samples/sample_01.jpeg'],
+            ['Oculus, Ukulele', 'MaskFormer', 1.0, './resources/demo_samples/sample_03.jpeg'],
+            ['Golden gate, yacht', 'MaskFormer', 1.0, './resources/demo_samples/sample_02.jpeg'],]
 output_labels = ['segmentation map']
 
-title = 'OVSeg'
+title = 'OVSeg (+ Segment_Anything)'
 
 description = """
+[NEW!] We incorperate OVSeg CLIP w/ Segment_Anything, enabling SAM's text prompts.
 Gradio Demo for Open-Vocabulary Semantic Segmentation with Mask-adapted CLIP. \n
 OVSeg could perform open vocabulary segmentation, you may input more classes (seperate by comma). You may click on of the examples or upload your own image. \n
 It might take some time to process. Cheers!
-<p>Don't want to wait in queue? <a href="https://colab.research.google.com/drive/1O4Ain5uFZNcQYUmDTG92DpEGCatga8K5?usp=sharing"><img data-canonical-src="https://colab.research.google.com/assets/colab-badge.svg" alt="Open In Colab" src="https://camo.githubusercontent.com/84f0493939e0c4de4e6dbe113251b4bfb5353e57134ffd9fcab6b8714514d4d1/68747470733a2f2f636f6c61622e72657365617263682e676f6f676c652e636f6d2f6173736574732f636f6c61622d62616467652e737667"></a></p>
+<p>(Colab only supports MaskFormer proposal generator) Don't want to wait in queue? <a href="https://colab.research.google.com/drive/1O4Ain5uFZNcQYUmDTG92DpEGCatga8K5?usp=sharing"><img data-canonical-src="https://colab.research.google.com/assets/colab-badge.svg" alt="Open In Colab" src="https://camo.githubusercontent.com/84f0493939e0c4de4e6dbe113251b4bfb5353e57134ffd9fcab6b8714514d4d1/68747470733a2f2f636f6c61622e72657365617263682e676f6f676c652e636f6d2f6173736574732f636f6c61622d62616467652e737667"></a></p>
 """
 
 article = """
@@ -80,7 +84,9 @@ gr.Interface(
     inputs=[
         gr.inputs.Textbox(
             lines=1, placeholder=None, default='', label='class names'),
-        gr.inputs.Image(type='filepath')
+        gr.inputs.Radio(["Segment_Anything", "MaskFormer"], label="Proposal generator", default="Segment_Anything"),
+        gr.inputs.Slider(0, 1.0, 0.8, label="For Segment_Anything, Granularity of masks from 0 (most coarse) to 1 (most precise)"),
+        gr.inputs.Image(type='filepath'),
     ],
     outputs=gr.outputs.Image(label='segmentation map'),
     title=title,
